ad952c041238f060781302726c4ff6f6b2ed2512
KonsoledSUFB <159624556+KonsoledSUFB@users.noreply.github.com>
2024-04-23 18:14:16 +0300

    код сделал для перилистывания
    смена сцен

---
+++ Gorilla Smash/Assets/scripts/scene1.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class scene1 : MonoBehaviour
+{
+    public  void ButtonPlay()
+    {
+        SceneManager.LoadScene(1);
+    }
+
+    public void ButtonBack()
+    {
+        SceneManager.LoadScene(0);
+    }
+}
