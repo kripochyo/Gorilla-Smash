4269ea416028bf645f0dc20bb325f829c6121706
kripochyo <demidbutor@gmail.com>
2024-04-28 20:34:19 +0500

    кнопочки
    сделал все кноаки на меню и лвл(выборе)

---
+++ Gorilla Smash/Assets/scripts/LevlController.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevlController : MonoBehaviour
+{
+    public void NextLevl()
+    {
+        var currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
+        SceneManager.LoadScene(currentSceneIndex + 1);
+    }
+}
---
+++ Gorilla Smash/Assets/scripts/scene4.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class scene4 : MonoBehaviour
+{
+    public void ButtonPlay3()
+    {
+        SceneManager.LoadScene(4);
+    }
+}
