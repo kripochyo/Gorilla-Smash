e7406a57845673bf6eb2eb62413a485bf0baa026
kripochyo <demidbutor@gmail.com>
2024-04-30 23:23:18 +0500

    теперь точно все

---
+++ Gorilla Smash/Assets/scripts/scene6.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class scene6 : MonoBehaviour
+{
+    public void ButtonPlaygaid()
+    {
+        SceneManager.LoadScene(6);
+    }
+}
