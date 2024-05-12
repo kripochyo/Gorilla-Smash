cc38c7b45165fe81f79faba5f343a12cd888de66
HIMEDY <159183385+HIMEDY@users.noreply.github.com>
2024-05-12 20:59:20 +0300

    делаю 2 лвл
    увеличил уровень, добавил секретную комнату в которой поместил дверь выхода из уровня

---
+++ Gorilla Smash/Assets/scripts/dlya2lvl.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class dlya2lvl : MonoBehaviour
+{
+    public GameObject walls;
+    public GameObject floor;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        Debug.Log("Collaider");
+        if (other.gameObject.GetComponent<PlatformerCharacterController>())
+        {
+            Debug.Log("Player");
+            Destroy(walls);
+            floor.SetActive(false);
+        }
+    }
+}
---
+++ Gorilla Smash/Assets/scripts/dlya2lvl2.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class dlya2lvl2 : MonoBehaviour
+{
+    public GameObject floor;
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.GetComponent<PlatformerCharacterController>())
+        {
+            floor.SetActive(true);
+        }
+    }
+}
