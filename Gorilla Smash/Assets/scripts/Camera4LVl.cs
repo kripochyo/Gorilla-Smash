a985737e3570f9dcb876a429c91f9a85c81b73c5
SigmaKripper <158759795+SigmaKripper@users.noreply.github.com>
2024-05-14 18:30:43 +0300

    Сделал 4 лвл (почти)

---
+++ Gorilla Smash/Assets/scripts/Camera4LVl.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Camera4LVl : MonoBehaviour
+{
+    public float minAngle;
+    public float maxAngle;
+    public Transform CameraAxisTransform;
+    public float RotationSpeed;
+    private void Update()
+    {
+        transform.localEulerAngles = new Vector3(0, transform.localEulerAngles.y + Time.deltaTime * RotationSpeed * Input.GetAxis("Mouse X"), 0);
+
+       
+    }
+}
