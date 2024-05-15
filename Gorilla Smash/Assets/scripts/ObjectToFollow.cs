4cd0f7aec0eeaa10e1485c1116f52674f7c1f98e
SigmaKripper <158759795+SigmaKripper@users.noreply.github.com>
2024-05-15 15:36:22 +0300

    дополнил свой уровень и немного ошибок в коде

---
+++ Gorilla Smash/Assets/scripts/ObjectToFollow.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Follow : MonoBehaviour
+{
+    public float speed = 5f;
+public Transform player;
+
+void Awake()
+{
+    player = GameObject.FindGameObjectWithTag(“Player”).GetComponent<Transform>();
+}
+
+
+void FixedUpdate()
+{
+    transform.position = Vector2.moveTowards(transform.position, player.position, speed);
+}
+}
+
+
+
