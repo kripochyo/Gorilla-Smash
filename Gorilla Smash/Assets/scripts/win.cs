54f169d2af485c93d6b246d0f913129fe818fc7a
kripochyo <demidbutor@gmail.com>
2024-05-18 19:05:17 +0500

    32131

---
+++ Gorilla Smash/Assets/scripts/win.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class win : MonoBehaviour
+{
+    private void OnTriggerEnter(Collider other)
+    {
+        var currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
+        SceneManager.LoadScene(0);
+    }
+}
