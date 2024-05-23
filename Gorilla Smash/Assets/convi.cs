de8ab0570a59157ec5fd7d8ce916a54b3dbf19fe
kripochyo <demidbutor@gmail.com>
2024-05-23 19:57:34 +0500

    3231312

---
+++ Gorilla Smash/Assets/convi.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using DialogueEditor;
+
+public class convi : MonoBehaviour
+{
+    [SerializeField] private NPCConversation myConvesation;
+    
+    private void OnTriggerStay(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            if (Input.GetKeyDown((KeyCode.E)))
+            {
+                ConversationManager.Instance.StartConversation(myConvesation);
+            }
+        }
+    }
+}
